cf3dad8487d58515488fc97e50303b0865f1fd40
alb <alberic.tosan@orange.fr>
2021-08-30 18:43:18 +0200

    some changes

--- books-handler-back/src/main/java/org/randy/bookshandler/back/DemoApplication.java
+++ books-handler-back/src/main/java/org/randy/bookshandler/back/DemoApplication.java
@@ -1,16 +1,17 @@
 package org.randy.bookshandler.back;
 
 import org.randy.bookshandler.openlibrary.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "org.randy.bookshandler")
+@SpringBootApplication(scanBasePackages = "org.randy")
 @RestController
 public class DemoApplication {
 
-  private final MyService myService;
+  private MyService myService;
 
   public DemoApplication(MyService myService) {
     this.myService = myService;
